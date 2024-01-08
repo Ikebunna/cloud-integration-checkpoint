608061616a2848cbdfdd302b118d901fc870bc18
IG <ifediorachukwu@yahoo.com>
2024-01-08 14:03:15 +0100

    final

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,28 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      Still Unable to locate us? Call <a href="">08011998495</a>
-    </div>
+    <>
+      <div>
+        Still Unable to locate us? Call <a href="">08011998495</a>
+      </div>
+      <br />
+      <div className="d-flex flex-column flex-sm-row justify-content-around">
+        <div>
+          <h5>Address</h5>
+          <p>
+            No 15 Kumasi Close <br /> New Layout <br /> California
+          </p>
+        </div>
+        <div>
+          <h5>Our Other Locations </h5>
+          <br />
+
+          <p>
+            Bauchi <br /> Maiduguri
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 
 const Map = () => {
-  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+  const LocPointer = ({ text }) => (
+    <>
+      <p>{text}</p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-bullseye"
+        viewBox="0 0 16 16"
+      >
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+        <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8" />
+        <path d="M9.5 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+      </svg>
+    </>
+  );
 
   // our location
   const defaultProps = {
@@ -20,7 +36,7 @@ const Map = () => {
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <AnyReactComponent lat={9.0760818} lng={7.4641472} text="042 Devs HQ" />
+        <LocPointer lat={9.0760818} lng={7.4641472} text="042 HQ" />
       </GoogleMapReact>
     </div>
   );
